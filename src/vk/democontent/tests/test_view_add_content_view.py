cfc3bc4db4eeffb72b84f9a725f26719d1e7c8a0
Verena Kunz-Meier <verena@kunz-meier.de>
2023-08-24 19:07:10 +0200

    "Add view: add-content-view"

---
+++ src/vk/democontent/tests/test_view_add_content_view.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from vk.democontent.testing import VK_DEMOCONTENT_FUNCTIONAL_TESTING
+from vk.democontent.testing import VK_DEMOCONTENT_INTEGRATION_TESTING
+from vk.democontent.views.add_content_view import IAddContentView
+from zope.component import getMultiAdapter
+from zope.interface.interfaces import ComponentLookupError
+
+import unittest
+
+
+class ViewsIntegrationTest(unittest.TestCase):
+
+    layer = VK_DEMOCONTENT_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+        api.content.create(self.portal, "Folder", "other-folder")
+        api.content.create(self.portal, "Document", "front-page")
+
+    def test_add_content_view_is_registered(self):
+        view = getMultiAdapter(
+            (self.portal["other-folder"], self.portal.REQUEST), name="add-content-view"
+        )
+        self.assertTrue(IAddContentView.providedBy(view))
+
+    def test_add_content_view_not_matching_interface(self):
+        view_found = True
+        try:
+            view = getMultiAdapter(
+                (self.portal["front-page"], self.portal.REQUEST),
+                name="add-content-view",
+            )
+        except ComponentLookupError:
+            view_found = False
+        else:
+            view_found = IAddContentView.providedBy(view)
+        self.assertFalse(view_found)
+
+
+class ViewsFunctionalTest(unittest.TestCase):
+
+    layer = VK_DEMOCONTENT_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
